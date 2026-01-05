891b9f704fad7c56b0f97de976be806faee85b2e
PabloRamosNTT <117345972+PabloRamosNTT@users.noreply.github.com>
2026-01-05 11:00:21 +0100

    Feature/new api aviation (#3)
    * build: added ignores and ladle
    
    * feat: add aviation tools and components for airplane listing
    
    * feat: add aviation widget and list components for airplane data

--- web/src/entries/component.tsx
+++ web/src/entries/component.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import type { PokemonType, Pokemon } from "../lib/types.js";
+import type { PokemonType, Pokemon, Airplane } from "../lib/types.js";
 import { useOpenAiGlobal } from "../lib/hooks.js";
+
+function AviationCard({ airplane }: { airplane: Airplane }) {
+  console.log("Card", airplane);
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "10px",
+        padding: "1rem",
+        textAlign: "center",
+      }}
+    >
+      {/* <img src={pokemon.img} alt={pokemon.name} width={100} height={100} /> */}
+      <h3 style={{ textTransform: "capitalize" }}>{airplane.production_line}</h3>
+      {/* <p>
+        <strong>Tipo:</strong>{" "}
+        {pokemon.types.map((t: PokemonType) => t.type.name).join(", ")}
+      </p> */}
+    </div>
+  );
+}
+
+function AviationList({ airplanes }: { airplanes: Airplane[] }) {
+  console.log("List", airplanes);
+  if (airplanes.length === 0) {
+    return <div>No hay airplanes</div>;
+  }
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))",
+        gap: "1rem",
+      }}
+    >
+      {airplanes.map((p: Airplane) => (
+        <AviationCard key={p.iata_code_long} airplane={p} />
+      ))}
+    </div>
+  );
+}
+
 
 function Card({ pokemon }: { pokemon: Pokemon }) {
   console.log("Card", pokemon);
@@ -52,12 +95,14 @@ export default function App() {
   const toolOutput = useOpenAiGlobal("toolOutput");
   console.error("toolOutput", toolOutput);
   const pokemons = toolOutput?.pokemonList ?? [];
+  const airplanes = toolOutput?.airplaneList ?? [];
   console.error("pokemons", pokemons);
 
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1 style={{ textAlign: "center" }}>Pokédex React {pokemons?.length}</h1>
-      <List pokemons={pokemons} />
+      {pokemons.length && <List pokemons={pokemons} />}
+      {airplanes.length && <AviationList airplanes={airplanes} />}
     </div>
   );
 }
