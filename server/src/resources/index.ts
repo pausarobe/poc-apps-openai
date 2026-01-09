293e2c1afe88fc8a5117553df0ef37910d110810
PabloRamosNTT <117345972+PabloRamosNTT@users.noreply.github.com>
2026-01-09 13:41:04 +0100

    chore: update and remove unused tool

--- server/src/resources/index.ts
+++ server/src/resources/index.ts
@@ -1,15 +1,8 @@
 import type { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
-import { registerAviationWidgetResource } from './aviation-widget.js';
 import { registerFlightDetailWidgetResource } from './flight-detail-widget.js';
 import { registerFlightDashboardWidgetResource } from './flight-dashboard-widget.js';
 
-// export function registerResources(server: McpServer, assets: { JS: string; JS_FLIGHT_DASHBOARD: string; JS_FLIGHT_DETAIL: string }) {
-//   registerAviationWidgetResource(server, assets.JS);
-//   registerFlightDashboardWidgetResource(server, assets.JS_FLIGHT_DASHBOARD);
-//   registerFlightDetailWidgetResource(server, assets.JS_FLIGHT_DETAIL);
-// }
 export function registerResources(server: McpServer, assets: { JS: string; CSS: string; JS_FLIGHT_DASHBOARD: string; JS_FLIGHT_DETAIL: string }) {
-  registerAviationWidgetResource(server, assets.JS);
   registerFlightDashboardWidgetResource(server, assets.JS_FLIGHT_DASHBOARD, assets.CSS);
   registerFlightDetailWidgetResource(server, assets.JS_FLIGHT_DETAIL, assets.CSS);
 }
--- server/src/tools/index.ts
+++ server/src/tools/index.ts
@@ -1,13 +1,9 @@
 import type { RegisterToolFn } from '../utils/types.js';
-import { registerAirplaneListTool } from './airplane-list.js';
-import { registerFlightTestListTool } from './flight-test-list.js';
+import { registerFlightDashboardTool } from './flight-dashboard.js';
 import { registerFlightDetailTool } from './flight-detail.js';
-// import { registerFlightDetailTool } from './flight-detail.js';
-
 
 export function registerTools(registerTool: RegisterToolFn) {
-  registerAirplaneListTool(registerTool);
-  registerFlightTestListTool(registerTool);
   registerFlightDetailTool(registerTool);
-  // registerFlightDetailTool(registerTool);
+  registerFlightDashboardTool(registerTool);
+
 }
