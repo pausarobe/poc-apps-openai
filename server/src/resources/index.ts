e1b1fcfc70703b0b79c2e846a22bf8ab82b99d64
pau-sarobe <psarobev@emeal.nttdata.com>
2026-01-13 13:23:52 +0100

    fix: loadassets

--- server/src/resources/index.ts
+++ server/src/resources/index.ts
@@ -3,7 +3,7 @@ import { registerFlightDetailWidgetResource } from './flight-detail-widget.js';
 import { registerFlightDashboardWidgetResource } from './flight-dashboard-widget.js';
 import { registerRentalCarListWidgetResource } from './rental-car-list-widget.js';
 
-export function registerResources(server: McpServer, assets: { JS: string; CSS: string; JS_FLIGHT_DETAIL: string; JS_RENTAL_CAR: string; JS_FLIGHT_DASHBOARD: string }) {
+export function registerResources(server: McpServer, assets: { CSS: string; JS_FLIGHT_DETAIL: string; JS_RENTAL_CAR: string; JS_FLIGHT_DASHBOARD: string }) {
   registerFlightDetailWidgetResource(server, assets.JS_FLIGHT_DETAIL, assets.CSS);
   registerFlightDashboardWidgetResource(server, assets.JS_FLIGHT_DASHBOARD, assets.CSS);
   registerRentalCarListWidgetResource(server, assets.JS_RENTAL_CAR, assets.CSS);
--- server/src/utils/assets.ts
+++ server/src/utils/assets.ts
@@ -3,10 +3,9 @@ import { join } from 'node:path';
 
 export function loadWebAssets(WEB_DIST: string) {
   return {
-    // JS: readFileSync(join(WEB_DIST, 'component.js'), 'utf8'),
     CSS: readFileSync(join(WEB_DIST, 'styles.css'), 'utf8'),
-    // JS_TEST: readFileSync(join(WEB_DIST, 'test.js'), 'utf8'),
     JS_FLIGHT_DETAIL: readFileSync(join(WEB_DIST, 'flight-detail.js'), 'utf8'),
     JS_FLIGHT_DASHBOARD: readFileSync(join(WEB_DIST, 'flight-dashboard.js'), 'utf8'),
+    JS_RENTAL_CAR: readFileSync(join(WEB_DIST, 'rental-car.js'), 'utf8'),
   };
 }
